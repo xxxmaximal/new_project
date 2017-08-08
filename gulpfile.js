356c7207a05b6f74da4096a97b1b1e2cc67386b8
Sergei Nechaev <xxxmaximal@mail.ru>
2017-08-09 02:03:10 +0300

    Удалена директория

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var browserSync = require("browser-sync").create();
+
+gulp.task('serve', ['less'], function() {
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch("less/*.less", ['less']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task("less", function() {
+    gulp.src("less/style.less")
+        .pipe(less())
+        .pipe(gulp.dest("css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
